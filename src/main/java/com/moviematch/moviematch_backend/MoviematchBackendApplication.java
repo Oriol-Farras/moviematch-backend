51428636146ab5005e7b41a6f3549291e942a281
Oriol-Farras <oriolfarras2004@gmail.com>
2025-11-04 19:14:52 +0100

    Initial commit

---
+++ src/main/java/com/moviematch/moviematch_backend/MoviematchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moviematch.moviematch_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviematchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviematchBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moviematch/moviematch_backend/MoviematchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviematch.moviematch_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviematchBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
