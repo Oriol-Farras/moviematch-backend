20dea3a13a2231c36d857357603dda0c9d6e922a
Oriol-Farras <oriolfarras2004@gmail.com>
2025-11-08 16:27:52 +0100

    test(app): add toggleable test to verify Spring Boot test setup

--- src/test/java/com/moviematch/moviematch_backend/MoviematchBackendApplicationTests.java
+++ src/test/java/com/moviematch/moviematch_backend/MoviematchBackendApplicationTests.java
@@ -2,12 +2,23 @@ package com.moviematch.moviematch_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class MoviematchBackendApplicationTests {
 
+	private final boolean shouldPass = true;
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void simpleToggleTest() {
+		if (shouldPass) {
+			assertTrue(true, "Test pass");
+		} else {
+			fail("Test fail");
+		}
 	}
 
 }
